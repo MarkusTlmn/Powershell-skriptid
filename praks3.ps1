f8967d5fa22ef1fa3524a24fba7dd81742d310fa
Markus Tellmann <markustellmann2@gmail.com>
2023-10-04 10:16:43 +0300

    praks3

---
+++ praks3.ps1
@@ -0,0 +1,9 @@
+#skripti algus
+
+#küsime kasutajalt sisendit
+$a = Read-Host "What is your name?"
+
+#väljastame antud vastuse rohelise värviga
+Write-Host $a -foregroundColor Green
+
+#skripti lõpp
