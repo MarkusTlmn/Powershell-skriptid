aa03cde17e5a7bbcdec7eb9f083355185c18aca8
Markus Tellmann <markustellmann2@gmail.com>
2023-10-04 09:58:04 +0300

    parandus

--- praks2.ps1
+++ praks2.ps1
@@ -3,10 +3,14 @@
 $hashtable1=@{Name="John","Joe","Mary"; DaysWorked=12,20,18}
 $hashtable2=@{Name="John","Joe","Mary"; SalaryPerDay=100,120,150}
 
-#
+#arvutame kõigi palga
 $Salary1 = $hashtable1.DaysWorked[0] * $hashtable2.SalaryPerDay[0]
 $Salary2 = $hashtable1.DaysWorked[1] * $hashtable2.SalaryPerDay[1]
 $Salary3 = $hashtable1.DaysWorked[2] * $hashtable2.SalaryPerDay[2]
 
+#loome uue tabeli palkade jaoks
 $hashtable3=@{Name="John","Joe","Mary"; Salary=$Salary1,$Salary2,$Salary3}
+#väljastame tabeli
 $hashtable3 
+
+#skripti lõpp
